f153e280878296ff6f82aae1f8e53ae347ac5249
Ioannis Tsiftzis <ioannis.tsiftzis@gmail.com>
2025-01-06 16:40:35 +0200

    for,if

--- main.go
+++ main.go
@@ -5,10 +5,22 @@ import (
 )
 
 func main() {
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
-
-	for i := 1; i <= 2; i++ {
-		fmt.Println("i =", 100/i)
+	checker := 5
+	for {
+		fmt.Printf("\nUer name: ")
+		var userName string
+		scan, err := fmt.Scan(&userName)
+		if err != nil {
+			fmt.Printf("Result: %d, Error:%f\n", scan, err)
+			return
+		}
+		var stores []int
+		fmt.Print(userName)
+		checker--
+		stores = append(stores, checker)
+		fmt.Println(stores)
+		if checker == 0 {
+			break
+		}
 	}
 }
